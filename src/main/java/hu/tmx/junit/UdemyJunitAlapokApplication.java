eac16dc4fcffa621f2abf79c68fe712b4ba4fef5
figlerr <figler.renata@tmx.hu>
2021-09-23 11:02:42 +0200

    forras

---
+++ src/main/java/hu/tmx/junit/UdemyJunitAlapokApplication.java
@@ -0,0 +1,13 @@
+package hu.tmx.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyJunitAlapokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyJunitAlapokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/tmx/junit/UdemyJunitAlapokApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.tmx.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyJunitAlapokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
