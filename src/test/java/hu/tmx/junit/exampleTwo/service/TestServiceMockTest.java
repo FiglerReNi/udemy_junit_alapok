e011d64f32ba66be0e183ed82e5c482f7043cd67
figlerr <figler.renata@tmx.hu>
2021-10-01 13:11:33 +0200

    forras

--- src/test/java/hu/tmx/junit/exampleTwo/service/TestServiceMockTest.java
+++ src/test/java/hu/tmx/junit/exampleTwo/service/TestServiceMockTest.java
@@ -1,6 +1,5 @@
 package hu.tmx.junit.exampleTwo.service;
 
-import hu.tmx.junit.UdemyJunitAlapokApplication;
 import hu.tmx.junit.exampleTwo.dao.TestDao;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,13 +7,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
-@ContextConfiguration(classes = UdemyJunitAlapokApplication.class)
 public class TestServiceMockTest {
 
     @InjectMocks
